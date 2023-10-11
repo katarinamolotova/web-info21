b6d3541d462acde379cbc9405efa823fec10514a
Кухтачев Михаил <MGKukhtachev@sberbank.ru>
2023-10-11 15:27:00 +0300

    add mapping in AllDataController

--- info21/src/main/java/edu/school21/info21/controllers/AllDataController.java
+++ info21/src/main/java/edu/school21/info21/controllers/AllDataController.java
@@ -1,58 +1,80 @@
 package edu.school21.info21.controllers;
 
+import edu.school21.info21.services.CheckServices;
+import edu.school21.info21.services.FriendsServices;
+import edu.school21.info21.services.P2pServices;
+import edu.school21.info21.services.PeerServices;
+import edu.school21.info21.services.RecommendationsServices;
+import edu.school21.info21.services.TaskServices;
+import edu.school21.info21.services.TimeTrackingServices;
+import edu.school21.info21.services.TransferredPointsServices;
+import edu.school21.info21.services.VerterServices;
+import edu.school21.info21.services.XpServices;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@AllArgsConstructor
 public class AllDataController {
+    private final PeerServices peerServices;
+    private final CheckServices checkServices;
+    private final FriendsServices friendsServices;
+    private final P2pServices p2pServices;
+    private final RecommendationsServices recommendationsServices;
+    private final TaskServices taskServices;
+    private final TimeTrackingServices timeTrackingServices;
+    private final TransferredPointsServices transferredPointsServices;
+    private final VerterServices verterServices;
+    private final XpServices xpServices;
 
-    @GetMapping("/1")
+    @GetMapping("/data/checks")
     public String getCheckTable(Model model) {
         return "index";
     }
 
-    @GetMapping("/2")
+    @GetMapping("/data/friends")
     public String getFriendsTable(Model model) {
         return "index";
     }
 
-    @GetMapping("/3")
+    @GetMapping("/data/p2p")
     public String getP2pTable(Model model) {
         return "index";
     }
 
-    @GetMapping("/4")
+    @GetMapping("/data/peers")
     public String getPeerTable(Model model) {
         return "index";
     }
 
-    @GetMapping("/5")
+    @GetMapping("/data/recommendations")
     public String getRecommendationsTable(Model model) {
         return "index";
     }
 
-    @GetMapping("/6")
+    @GetMapping("/data/tasks")
     public String getTaskTable(Model model) {
         return "index";
     }
 
-    @GetMapping("/7")
+    @GetMapping("/data/time-tracking")
     public String getTimeTrackingTable(Model model) {
         return "index";
     }
 
-    @GetMapping("/8")
+    @GetMapping("/data/transferred-points")
     public String getTransferredTable(Model model) {
         return "index";
     }
 
-    @GetMapping("/9")
+    @GetMapping("/data/verter")
     public String getVerterTable(Model model) {
         return "index";
     }
 
-    @GetMapping("/10")
+    @GetMapping("/data/xp")
     public String getXpTable(Model model) {
         return "index";
     }
