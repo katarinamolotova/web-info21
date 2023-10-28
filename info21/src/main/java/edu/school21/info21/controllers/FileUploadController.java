101ec787298289b8f98f5044adbaf24eee94fac6
Кухтачев Михаил <MGKukhtachev@sberbank.ru>
2023-10-28 22:48:19 +0300

    fix controller

--- info21/src/main/java/edu/school21/info21/controllers/FileUploadController.java
+++ info21/src/main/java/edu/school21/info21/controllers/FileUploadController.java
@@ -1,8 +1,5 @@
 package edu.school21.info21.controllers;
 
-import com.google.common.base.Enums;
-import com.google.common.base.Optional;
-import edu.school21.info21.enums.TableNames;
 import edu.school21.info21.services.IOFileService;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
@@ -11,16 +8,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
-@RestController
+@Controller
 public class FileUploadController {
     private final IOFileService service;
 
