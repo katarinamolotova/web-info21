a823604fff088cbaf5711178e551cd4e243cd367
Кухтачев Михаил <MGKukhtachev@sberbank.ru>
2023-10-29 12:29:22 +0300

    refactor

--- info21/src/main/java/edu/school21/info21/controllers/FileIOController.java
+++ info21/src/main/java/edu/school21/info21/controllers/FileIOController.java
@@ -4,9 +4,6 @@ import edu.school21.info21.services.IOFileService;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
@@ -29,7 +26,7 @@ public class FileIOController {
         return service.fileUpload(name, file);
     }
 
-    @RequestMapping(value = "/download", method = RequestMethod.POST)
+    @PostMapping(value = "/download")
     public @ResponseBody
     void getFile(final HttpServletResponse response,
                  @RequestParam("table_name") final String table,
