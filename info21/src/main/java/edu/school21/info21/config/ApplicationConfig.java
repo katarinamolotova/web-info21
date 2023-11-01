315fa090ec1b0599499f4ddf56ae5accfb7e175e
Кухтачев Михаил <MGKukhtachev@sberbank.ru>
2023-11-01 09:51:12 +0300

    clear comments

--- info21/src/main/java/edu/school21/info21/config/ApplicationConfig.java
+++ info21/src/main/java/edu/school21/info21/config/ApplicationConfig.java
@@ -12,8 +12,6 @@ public class ApplicationConfig {
     @Bean
     MultipartConfigElement multipartConfigElement() {
         MultipartConfigFactory factory = new MultipartConfigFactory();
-//        factory.setMaxFileSize("128KB");
-//        factory.setMaxRequestSize("128KB");
         factory.setMaxFileSize(DataSize.ofBytes(1000000));
         factory.setMaxRequestSize(DataSize.ofBytes(1000000));
         return factory.createMultipartConfig();
